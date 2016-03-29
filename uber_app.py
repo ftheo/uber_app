241c0670cf41ac425f198c983938e56a564af762
Filippos Theodorakis <ftheodorakis@gmail.com>
2016-03-29 10:17:15 -0700

    Initial commit

---
+++ uber_app.py
@@ -0,0 +1,33 @@
+from flask import Flask
+from flask import request
+from threading import Thread
+import random
+import string
+
+
+app = Flask(__name__)
+
+@app.route('/')
+def hello_world():
+    return 'Hello World!'
+
+@app.route('/dispatch', methods=['GET'])
+def upload_file():
+    task_time = int(request.args.get('time', ''))
+    thr = Thread(target=dispatch, args=[app, task_time])
+    thr.start()
+    return str(task_time)
+
+def dispatch(app, task_time):
+    import time
+    id = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(6))
+    print "task " + id + " started"
+    end_time = int(time.time()) + task_time
+    while int(time.time()) < end_time:
+        pass
+    print "task " + id + " completed"
+
+
+if __name__ == '__main__':
+    # app.debug = True
+    app.run(host='0.0.0.0')
